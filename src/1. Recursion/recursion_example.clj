7e273c72d108ad22054c31636a42239898b28ec0
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2017-08-22 17:00:05 -0500

    countdown function using explicit recursion.

--- src/1. Recursion/recursion_example.clj
+++ src/1. Recursion/recursion_example.clj
@@ -25,3 +25,10 @@
     (if (<= i 1)
       accum
       (recur (inc accum) (quot i 2)))))
+
+(defn countdown
+  "Returns the list with: n, n-1, n-2, ..., 2, 1."
+  [n]
+  (if (< n 1)
+    ()
+    (cons n (countdown (dec n)))))
