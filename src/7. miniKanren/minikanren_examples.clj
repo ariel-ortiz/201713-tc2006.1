3cecc7a26bebae5b71e732854ad99832c9764563
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2017-11-17 18:56:48 -0600

    More examples of logical functions.

--- src/7. miniKanren/minikanren_examples.clj
+++ src/7. miniKanren/minikanren_examples.clj
@@ -22,3 +22,25 @@
        (conso head tail lst)
        (reverseo tail temp)
        (appendo temp [head] result))]))
+
+(defn dupo
+  "Logical function that succeeds if every element in lst is
+  duplicated in result."
+  [lst result]
+  (conde
+    [(=== lst ())
+     (=== result ())]
+
+    [(fresh [head tail temp]
+       (conso head tail lst)
+       (dupo tail temp)
+       (appendo [head head] temp result))]))
+
+(defn twin-dupleo
+  "Logical function that succeeds if lst is a two element
+  sequence where both elements are actually the same."
+  [lst]
+  (conde
+    [(fresh [h1 t1]
+       (conso h1 t1 lst)
+       (conso h1 () t1))]))
